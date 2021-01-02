09ee73dc10a3bcac031a109a1fff6509b14c3065
kirilproger <kirilproger@gmail.com>
2021-01-02 14:55:01 +0300

    добавил отправку запроса к базе

--- CostManager/addwindow.cpp
+++ CostManager/addwindow.cpp
@@ -16,14 +16,23 @@ addwindow::~addwindow()
 void addwindow::on_pushButton_clicked()
 {
     QSqlQuery addquery;
-    int day=0,month=0,year=0;
+    QString day,month,year;
     QString name,type;
     int price=0,amount=0;
-    day = ui->dayBox->currentText().toInt();
-    month = ui->monthBox->currentText().toInt();
-    year = ui->yearBox->currentText().toInt();
+    day = ui->dayBox->currentText();
+    month = ui->monthBox->currentText();
+    year = ui->yearBox->currentText();
     name = ui->NameEdit->text();
     type = ui->typeBox->currentText();
     price = ui->PriceEdit->text().toInt();
     amount = ui->AmountEdit->text().toInt();
+    addquery.prepare("INSERT INTO spending (ID,THEDATE,NAME,PRICE,AMOUNT,TYPE)"
+                     "VALUES(:ID, :THEDATE, :NAME, :PRICE, :AMOUNT, :TYPE)");
+    addquery.bindValue(":ID",2);
+    addquery.bindValue(":NAME",name);
+    addquery.bindValue(":THEDATE",year+"-"+month+"-"+day);
+    addquery.bindValue(":PRICE",price);
+    addquery.bindValue(":AMOUNT",amount);
+    addquery.bindValue(":TYPE",type);
+    addquery.exec();
 }
--- CostManager/mainwindow.cpp
+++ CostManager/mainwindow.cpp
@@ -13,7 +13,7 @@ MainWindow::MainWindow(QWidget *parent) :
     tWindow = new TuneWindow();
     default_connection();
     QSqlQuery query;
-    query.exec("CREATE TABLE PRODUCTS(ID INT,NAME VARCHAR(20), AMOUNT INT,TYPE INT, PRIMARY KEY(ID))");
+    query.exec("CREATE TABLE SPENDING(ID INT,THEDATE DATE,NAME VARCHAR(20),PRICE INT, AMOUNT INT,TYPE VARCHAR(20), PRIMARY KEY(ID))");
 }
 
 MainWindow::~MainWindow()
