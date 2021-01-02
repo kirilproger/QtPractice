863339a0d03ca35ac3a8f79b540496a5e5eb50c9
kirilproger <kirilproger@gmail.com>
2021-01-02 20:11:16 +0300

    функция добавления работает

--- CostManager/addwindow.cpp
+++ CostManager/addwindow.cpp
@@ -15,6 +15,11 @@ addwindow::~addwindow()
 
 void addwindow::on_pushButton_clicked()
 {
+    QSqlQuery lastindex;
+    lastindex.exec("SELECT MAX(ID) FROM spending");
+    int lidx;
+    lastindex.first();
+    lidx=lastindex.value(0).toInt()+1;
     QSqlQuery addquery;
     QString day,month,year;
     QString name,type;
@@ -26,13 +31,14 @@ void addwindow::on_pushButton_clicked()
     type = ui->typeBox->currentText();
     price = ui->PriceEdit->text().toInt();
     amount = ui->AmountEdit->text().toInt();
-    addquery.prepare("INSERT INTO spending (ID,THEDATE,NAME,PRICE,AMOUNT,TYPE)"
-                     "VALUES(:ID, :THEDATE, :NAME, :PRICE, :AMOUNT, :TYPE)");
-    addquery.bindValue(":ID",2);
+    addquery.prepare("INSERT INTO spending (ID,THEDATE,NAME,PRICE,AMOUNT,COST,TYPE)"
+                     "VALUES(:ID, :THEDATE, :NAME, :PRICE, :AMOUNT,:COST, :TYPE)");
+    addquery.bindValue(":ID",lidx);
     addquery.bindValue(":NAME",name);
     addquery.bindValue(":THEDATE",year+"-"+month+"-"+day);
     addquery.bindValue(":PRICE",price);
     addquery.bindValue(":AMOUNT",amount);
+    addquery.bindValue(":COST",price*amount);
     addquery.bindValue(":TYPE",type);
     addquery.exec();
 }
